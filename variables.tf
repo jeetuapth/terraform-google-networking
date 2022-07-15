7407e6e7b69da5c27ac05c5cfaebe29b94ab5159
Jitendra Singh Lodhi <63745741+jeetuapth@users.noreply.github.com>
2022-07-15 21:54:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,26 +9,26 @@ variable "project" {
 variable "region" {
   type = string
   description = "GCP region"
-  default     = "us-central"
+  default     = null
 }
 # define GCP zone
 variable "zone" {
   type = string
   description = "GCP zone"
-  default     = "us-central1"
+  default     = null
 }
 
 # define private subnet
 variable "private_subnet_cidr_1" {
   type = string
   description = "private_subnet_CIDR 1"
-  default     = "null"
+  default     = null
 }
 
 # define application name
 variable "app_name" {
   type = string
   description = "Application name"
-  default     = "null"
+  default     = null
 }
 
