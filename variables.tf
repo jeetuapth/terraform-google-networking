da4fdbf0da9f6bac8a05050d43170d51494cec01
Jitendra Singh Lodhi <63745741+jeetuapth@users.noreply.github.com>
2022-07-16 21:14:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,14 +17,25 @@ variable "zone" {
   description = "GCP zone"
   default     = null
 }
-
+/*
 # define private subnet
 variable "private_subnet_cidr_1" {
   type = string
   description = "private_subnet_CIDR 1"
   default     = null
 }
-
+*/
+  
+variable "private-subnet" {
+  description = "Create private subnets with these names"
+  type        = list(string)
+  default     = null
+}
+variable "private_subnet_cidr" {
+  description = "Create private subnets cidr with these names"
+  type        = list(string)
+  default     = null
+  
 # define application name
 variable "app_name" {
   type = string
